30e103eac649dc62dd4066749aeacd9ec79f1f9d
Justin Smith <jvstinian@gmail.com>
2021-11-23 11:03:28 -0500

    Making error tolerance more consistent in tests.

--- src/test/java/TestBetaDistributionCDF.java
+++ src/test/java/TestBetaDistributionCDF.java
@@ -15,7 +15,7 @@ public class TestBetaDistributionCDF {
 
       assertTrue(Math.abs(cdf.value(0.1) - 0.2048327647) < 1e-10);
       assertTrue(Math.abs(cdf.value(0.25) - 0.3333333333) < 1e-10);
-      assertTrue(Math.abs(cdf.value(0.5) - 0.5) < 1e-8);
+      assertTrue(Math.abs(cdf.value(0.5) - 0.5) < 1e-10);
       assertTrue(Math.abs(cdf.value(0.75) - 0.6666666667) < 1e-10);
       assertTrue(Math.abs(cdf.value(0.9) - 0.7951672353) < 1e-10);
     } catch (InvalidParameterException e) {
--- src/test/java/TestBetaDistributionPDF.java
+++ src/test/java/TestBetaDistributionPDF.java
@@ -15,7 +15,7 @@ public class TestBetaDistributionPDF {
 
       assertTrue(Math.abs(cdf.value(0.1) - 1.061032954) < 1e-10);
       assertTrue(Math.abs(cdf.value(0.25) - 0.7351051939) < 1e-10);
-      assertTrue(Math.abs(cdf.value(0.5) - 0.6366197723) < 1e-8);
+      assertTrue(Math.abs(cdf.value(0.5) - 0.6366197723) < 1e-10);
       assertTrue(Math.abs(cdf.value(0.75) - 0.7351051939) < 1e-10);
       assertTrue(Math.abs(cdf.value(0.9) - 1.061032954) < 1e-10);
     } catch (InvalidParameterException e) {
